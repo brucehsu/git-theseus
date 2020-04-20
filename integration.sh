1195c54dcac43dd35ec9ae24cb1ef6fe6699ee13
Szu-Kai Hsu (@brucehsu) <brucehsu@gmail.com>
2020-04-20 23:40:35 +0200

    ci: unzip always overwrites

--- integration.sh
+++ integration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd tests/integration/mock_repo
-unzip repo.zip # a hack in order to preserve .git
+unzip -o repo.zip # a hack in order to preserve .git
 
 RESULT=$(../../../git-theseus a5660be67b5d1b883d44a5f57c7722abe59079af addition.rb 3 418c8b04c6070b2c08fff937b3cd59f39bb8654c)
 [[ $RESULT == "Given code found in addition.rb#L8-8" ]] && echo "[ PASSED ] addition.rb" || echo "[ FAILED ] addition.rb: $RESULT"
